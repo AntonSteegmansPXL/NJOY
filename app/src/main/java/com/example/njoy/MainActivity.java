54af80d0c0568b5360b65fb7bc74b191d6ebc336
Anton Steegmans <11902529@student.pxl.be>
2022-01-13 23:42:54 +0100

    Added menu to second activity

--- app/src/main/java/com/example/njoy/MainActivity.java
+++ app/src/main/java/com/example/njoy/MainActivity.java
@@ -1,38 +1,24 @@
 package com.example.njoy;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mEventTextView;
     private Button secondActivityBtn;
-    //private Button button;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*mEventTextView = (TextView) findViewById(R.id.eventList);
-
-        for (int i = 1; i <25; i++) {
-            mEventTextView.append("EVENT " + i + "\n\n\n");
-        }*/
         secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
         secondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,16 +26,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
-
-
-
-        /*button = (Button) findViewById(R.id.settingsBtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-            }
-        });*/
     }
 
     @Override
@@ -78,8 +54,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onButtonClick(android.view.View view) {
     }
 }
--- app/src/main/java/com/example/njoy/SecondActivity.java
+++ app/src/main/java/com/example/njoy/SecondActivity.java
@@ -2,12 +2,14 @@ package com.example.njoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -31,17 +33,38 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {  //BACKEND FOR BACK BUTTON!
-        switch (item.getItemId()) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            Context context = SecondActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemThatWasClickedId == R.id.action_help) {
+            Context context = SecondActivity.this;
+            String textToShow = "Help clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemThatWasClickedId == R.id.action_settings) {
+            startActivity(new Intent(SecondActivity.this, SettingsActivity.class));
+            Context context = SecondActivity.this;
+            String textToShow = "Opening settings";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+        }
+
+        switch (item.getItemId()) { //BACKEND FOR BACK BUTTON!
             case android.R.id.home:
                 finish();
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
     }
 }
