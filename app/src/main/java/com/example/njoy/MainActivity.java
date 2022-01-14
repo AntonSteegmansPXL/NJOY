dc5ad6cb875fe30b141425780aa7ac6d800846a7
Anton Steegmans <11902529@student.pxl.be>
2022-01-14 20:29:47 +0100

    started designing UI, added colors to resources

--- app/src/main/java/com/example/njoy/MainActivity.java
+++ app/src/main/java/com/example/njoy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.njoy;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
         secondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
